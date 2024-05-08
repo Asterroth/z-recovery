04ecf42937f8384ab14cd889acf9b9f1f08df417
Asterroth <anton.malukhin@gmail.com>
2024-05-09 00:53:05 +0300

    update app install

---
+++ web-app2/app-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+if [ `id -u` != 0 ]
+then
+    echo "$0: Run this script as root!"
+    exit 1
+fi
+
+html_path='/var/www/html'
+ports_conf_dir='/etc/apache2'
+ports_conf='/etc/apache2/ports.conf'
+default_conf_dir='/etc/apache2/sites-enabled'
+default_conf='/etc/apache2/sites-enabled/000-default.conf'
+
+apt install -yq apache2
+mkdir $html_path
+
+rm $html_path/index.html
+cp ./config/index2.html $html_path/index.html
+
+mv $ports_conf $ports_conf_dir/ports.conf.original
+cp ./config/ports2.conf $ports_conf
+
+mv $default_conf $default_conf_dir/000-default.conf.original
+cp ./config/000-default2.conf $default_conf_dir/000-default.conf
+
+systemctl enable --now apache2
---
+++ web-app2/app-network.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+if [ `id -u` != 0 ]
+then
+    echo "$0: Run this script as root!"
+    exit 1
+fi
+
+
+# netplan
+netplan_etc_dir='/etc/netplan'
+netplan_src_path='./config/00-app-config.yaml'
+netplan_cfg_name='00-app-config.yaml'
+hostname='app2'
+
+new_ip4rules='./config/app-net-rules.sh'
+etc_ip4_rules='/etc/iptables/rules.v4'
+
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+apt -yq install iptables-persistent
+$new_ip4rules
+iptables-save > $etc_ip4_rules
+
+
+# Update network settings
+echo $hostname > /etc/hostname
+hostname $hostname
+rm $netplan_etc_dir/* || true
+cp $netplan_src_path $netplan_etc_dir/$netplan_cfg_name
+netplan apply
