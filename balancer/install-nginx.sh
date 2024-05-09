8bfde4d4144e28bb7bae6c76949a07c771443e67
Asterroth <anton.malukhin@gmail.com>
2024-05-09 17:55:25 +0300

    update balancer

--- balancer/install-nginx.sh
+++ balancer/install-nginx.sh
@@ -9,7 +9,7 @@ fi
 sites_avail='/etc/nginx/sites-available'
 sites_enabl='/etc/nginx/sites-enabled'
 nginx_src_proxy_conf='./config/balancer-upstream.conf'
-nginx_trg_proxy_conf=$sites_avail/nginx-web-app-upstreamed.conf
+nginx_trg_proxy_conf=$sites_avail/default
 nginx_default_conf=$sites_enabl/default
 # prometheus-nginx-exporter
 ####ng_exp_sysd_trg='/lib/systemd/system/prometheus-nginx-exporter.service'
@@ -22,11 +22,11 @@ nginx_default_conf=$sites_enabl/default
 # Install nginx
 apt -yq install nginx
 
-
 [ ! -L "$nginx_default_conf" ] || rm -f "$nginx_default_conf"
 cp "$nginx_src_proxy_conf" "$nginx_trg_proxy_conf"
 chmod ugo+r "$nginx_trg_proxy_conf"
 ln -s -f "$nginx_trg_proxy_conf" "$etc_sites_enabl/"
+[ ! -L "$nginx_default_conf" ] || rm -f "$nginx_default_conf"
 
 systemctl enable nginx
 systemctl restart nginx
