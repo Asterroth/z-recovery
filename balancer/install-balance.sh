d3bebac36c1cf5b32013c99dfb72d810d6dcdc4d
Asterroth <anton.malukhin@gmail.com>
2024-06-01 10:38:27 +0300

    update balancer

--- balancer/install-balance.sh
+++ balancer/install-balance.sh
@@ -11,34 +11,20 @@ sites_enabl='/etc/nginx/sites-enabled'
 nginx_src_proxy_conf='./config/balancer-upstream.conf'
 nginx_trg_proxy_conf=$sites_avail/default
 nginx_default_conf=$sites_enabl/default
-# prometheus-nginx-exporter
-####ng_exp_sysd_trg='/lib/systemd/system/prometheus-nginx-exporter.service'
-####ng_exp_sysd_src='./prometheus-nginx-exporter-cfg/prometheus-nginx-exporter.service'
-# iptables
-###new_ip4rules='./config/balancer-net-rules.sh'
-###ip4_rules='/etc/iptables/rules.v4'
 
+apt install -yq apache2 nfs-common
 
-# Install nginx
-fstab_record='192.168.100.188:/mnt/backup-data   /mnt/backup-data   nfs   defaults,timeo=300,retrans=5,_netdev	0 0'
-
-apt install -yq nginx nfs-common
-
-mkdir /mnt/backup-data
-echo $fstab_record >> /etc/fstab
-mount 192.168.100.188:/mnt/backup-data /mnt/backup-data/
+../common/mount-nfs.sh
 
 cp $nginx_src_proxy_conf $nginx_trg_proxy_conf
 chmod ugo+r $nginx_trg_proxy_conf
 ln -s -f $nginx_trg_proxy_conf $etc_sites_enabl/
-#[ ! -L $nginx_default_conf ] || rm -f $nginx_default_conf
 
 systemctl enable nginx
 systemctl restart nginx
 
 apt -yq install prometheus-node-exporter
 systemctl enable --now prometheus-node-exporter
-#######
 
 dpkg -i /mnt/backup-data/filebeat_8.9.1_amd64-224190-507082.deb
 apt --fix-broken -yqq install
--- balancer/network-balancer.sh
+++ balancer/network-balancer.sh
@@ -8,9 +8,8 @@ fi
 
 
 # netplan
-netplan_etc_dir='/etc/netplan'
-netplan_src_path='./config/00-balancer-config.yaml'
-netplan_cfg_name='00-balancer-config.yaml'
+netplan_conf_dir='/etc/netplan'
+netplan_yml='./config/00-balancer-network-config.yaml'
 hostname='balancer'
 
 new_ip4rules='./config/balancer-net-rules.sh'
@@ -18,18 +17,18 @@ etc_ip4_rules='/etc/iptables/rules.v4'
 
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-apt -yq install iptables-persistent
+apt -yqq install iptables-persistent
 $new_ip4rules
 iptables-save > $etc_ip4_rules
 
-
-# Update network settings
-echo $hostname > /etc/hostname
 hostname $hostname
-rm $netplan_etc_dir/* || true
-cp $netplan_src_path $netplan_etc_dir/$netplan_cfg_name
+
+[ ! -e $netplan_conf_dir/* ] || rm $netplan_conf_dir/*
+cp $netplan_yml $netplan_conf_dir/
+netplan apply
+
 echo "*****"
 echo "New network configuration applied!"
 echo "----------------------------------"
 echo "  Please re-connect with new IP!  "
-netplan apply
+
